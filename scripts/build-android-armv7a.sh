80ae5f8f034afcdb0a9a0c70e7ac451d163ad620
zzh <118289872+2376780283@users.noreply.github.com>
2024-09-05 21:42:25 +0800

    Update build-android-armv7a.sh

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 git submodule init && git submodule update
-wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
-unzip android-ndk-r10e-linux-x86_64.zip
-export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -o /dev/null
+
+sudo apt-get update
+sudo apt-get install -f -y libopenal-dev g++-multilib gcc-multilib libpng-dev libjpeg-dev libfreetype6-dev libfontconfig1-dev libcurl4-gnutls-dev libsdl2-dev zlib1g-dev libbz2-dev libedit-dev
+
+wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -nc -q    
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -nc -q
+
+unzip -q android-ndk-r10e-linux-x86_64.zip
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
-./waf configure -T release --prefix=modlauncher-waf/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --disable-warns -4 --build-game=lmod &&
-./waf install --target=client,server,engine --strip
-# 移除多余lib
-rm modlauncher-waf/android/lib/armeabi-v7a/libtier0.so
-rm modlauncher-waf/android/lib/armeabi-v7a/libsteam_api.so
-rm modlauncher-waf/android/lib/armeabi-v7a/libvstdlib.so
-# 加引擎脑子纯粹喀了屎
+
+export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
+export PATH=$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH
+
+python3 ./waf configure -T release --prefix=../android_build --android=aarch64,host,21 --target=../android_build/aarch64 --disable-warns --togles --GAMES=episodic
+python3 ./waf install --strip
